a9e0e54eda7382ac8f3e48250e72e4aa4453fef6
postlainaldo <postlain.aldo@gmail.com>
2026-06-20 22:20:30 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         black: "#000000",
-        darkAccent: "#121212",
+        darkBg: "#0A0A0C",
         appleBorder: "rgba(255, 255, 255, 0.08)",
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+        serif: ["var(--font-playfair)", "serif"],
       },
     },
   },
