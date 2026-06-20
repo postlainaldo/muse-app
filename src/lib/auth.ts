307881d5a019d0464d7a82187cc8856ee41bbddf
postlainaldo <postlain.aldo@gmail.com>
2026-06-20 22:26:31 +0700

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,30 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: any = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "openid email profile https://www.googleapis.com/auth/drive.file",
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, account }: any) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      return session;
+    }
+  }
+};
