ae67a4ce4d756e9764109d7d5841a8c738b23d9f
postlainaldo <postlain.aldo@gmail.com>
2026-06-20 22:51:51 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Quét rộng hơn toàn bộ thư mục src
+  ],
   theme: {
     extend: {
       colors: {
